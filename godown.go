3d6a14c11873ee8eebdb7cd60da5877aa38c5725
Javed Khan <javed@agiliq.com>
2013-05-21 00:03:34 +0530

    yippee

--- godown.go
+++ godown.go
@@ -1,5 +1,6 @@
 package main
 
+import "io"
 import "os"
 import "fmt"
 import "path"
@@ -9,25 +10,22 @@ import "net/url"
 import "net/http"
 import "io/ioutil"
 
-func download(uri string, chunks chan int, offset int, file *os.File) {
-    for current := range chunks {
+func download(uri string, start int, offset int, chunks chan []byte) {
+    fmt.Println("Downloading range: ", start, "-", start+offset)
 
-        fmt.Println("Downloading range: ", current, "-", current+offset)
-
-        client := &http.Client{}
-        req, _ := http.NewRequest("GET", uri, nil)
-        req.Header.Set("Range: ", fmt.Sprintf("bytes=%d-%d", current, current+offset))
-        resp, err := client.Do(req)
-        if err != nil {
-            panic(err)
-        }
-        defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            panic(err)
-        }
-        file.Write(body)
+    client := &http.Client{}
+    req, _ := http.NewRequest("GET", uri, nil)
+    req.Header.Set("Range: ", fmt.Sprintf("bytes=%d-%d", start, start+offset))
+    resp, err := client.Do(req)
+    if err == io.EOF {
+        return
     }
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    chunks <- body
 }
 
 func main() {
@@ -57,15 +55,14 @@ func main() {
 
     content_length, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
     offset := content_length / *threads
-    current := 0
+    start := 0
 
-    chunks := make(chan int)
-
-    go download(*download_url, chunks, offset, file)
+    chunks := make(chan []byte)
 
     for _ = range make([]int, *threads) {
-        chunks <- current
-        current += offset
+        go download(*download_url, start, offset, chunks)
+        start += offset
     }
+    file.Write(<-chunks)
     fmt.Println("Download complete - saved to", filename)
 }
